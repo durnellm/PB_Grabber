3d9aa37cf66824cbb40ce7c6f6d29edec381debf
durnellm <50713265+durnellm@users.noreply.github.com>
2020-03-16 19:11:24 -0400

    Update SRDC.py

--- SRDC.py
+++ SRDC.py
@@ -9,10 +9,18 @@ from datetime import timedelta as tdel
 from datetime import time
 import math
 
-
+global perm 
+perm = True
 Client = discord.Client()
 bot_prefix= "."
 client = commands.Bot(command_prefix=bot_prefix)
+
+def check():
+	return perm
+
+def change():
+	global perm
+	perm = False
 
 def convert(t):
 	a = int(t/60/60)
@@ -23,44 +31,76 @@ def convert(t):
 
 ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(math.floor(n/10)%10!=1)*(n%10<4)*n%10::4])
 
-boards = ['9d8v46kn','zd36qvdn','xd1mg7d8','zdnlex2q','9kvmn8kg','vdo0w8vd','xk9gvw4d','n2y17z2o','wkpqjvdr','ndxr01dq','wdmw6ro2','wkpo3nw2','02qpyqpd','vdoo1yvd','vdo3wpod','xd1701zd','7dgr6ypk','02qj3jdy','xd1myzd8','jdz5w3dv','824x9wgd','9d8pnr3k','xd1lm8k8','wk6vnxod','9d8p8n7k','824rlogd','zdnprxkq','xk9lv4k0','n2y7op72','7kj103gk','xk98zrx2','7dggn7d4','7dggz8xd','wk6jrxd1','zd3yx82n','7dgjp4d4','5dwp4ekg','q258j8j2','xd17168d','zd3045nd','xk9gp4d0','n2y1yzm2','q25o9r8k','jdrq8jxk','jdrpvg26','9d86l32n','jdz593dv','vdoqv09k','vdo08gvd','xd1e5428','z2761w0d','xk9l84k0','ndxm4nr2','w20w3n5d','xd13zprk','z27l8pgd','zdnqxedq','q2543ydo','n2y3x9ed','rklxy56k','wk61mnxd','zd3yw82n','zdn15xkq','wkpy1gkr','9d8gow3k','ndx895kq','w20zg8dn','wk6vj1qd','n2y71jz2','z27lr5gd','wdmqx52q','mkerv6nd','02qjlqzd','mke5z48k','n2y39y1d','xk9nemg2','7kjq8nzd','wdml1wek','7dg6olk4','mke598k6','z27lryod','xd1l4m8k','jdry0026','jdzx4x2v','n2y1ple2','wk6ownrk','wkpyr1jk','7kjrynn2','ndx86o5k','jdrewgk6','wk6jnqd1','w204ovkn','wk6vrqd1','n2y1oz2o','7kjr3n23','xk9lr4k0','mkez0qnk','w20w5qvd','wdmw75x2','vdoon51d','wkpor502','7dgr7mxk','mkez7xxk','wdmw1j52','7dgrep7k','mkezpm8k','5dw5ezed','7dggjzpd','5dwpw0lk','wkpyx8gk','mke56e6k','xd1mqlzd','5dwpwplk','wk610yxd','rklgzv6d','9kvxv932','zd3yr8e2','ndx807ok','w20z5vjd','wdmq7le2','vdo4n862','9kvxl7e2','n2yo34md']
-
+boards = []
+boardlist = open("boards.txt", "r")
+boardtemp = boardlist.readlines()
+for i in boardtemp:
+	boards.append(i[:8])
+	
 @client.event
 async def on_ready():
-	if len(boards)==123:
-		channel = client.get_channel(597203792055894016)
-		#channel = client.get_channel(557428718247870492)
-		t2 = dtime.utcnow()-tdel(minutes = 15)
-		new_runs = []
-		for board in boards:
-		    cat = api.get("runs?category="+board+"&max=500")
-		    for j in cat:
-		        if (j['status']['status']=='verified' and j['status']['verify-date'] is not None):
-		            vtime = dtime.strptime(j['status']['verify-date'],'%Y-%m-%dT%H:%M:%SZ')
-		            if (vtime>=t2):
-		                new_runs.append(j['id'])
-		boards.append('0')
-		if len(new_runs)>0:
-			for i in new_runs:
-			    p = 0
-			    r = api.get("runs/"+i)
-			    n = api.get('users/'+r['players'][0]['id'])['names']['international']
-			    g = api.get('games/'+r['game'])['names']['international']
-			    c = g+" "+api.get('categories/'+r['category'])['name']
-			    t = convert(int(r['times']['primary_t'])).strftime('%H:%M:%S')
-			    if r['level'] is None:
-			        b = api.get('leaderboards/'+r['game']+'/category/'+r['category'])
-			        for j in range(len(b['runs'])):
-			            if b['runs'][j]['run']['id']==i:
-			                p = b['runs'][j]['place']
-			        if p ==1:
-			        	m = "<:GoldScar:619662499381379072> ["+n+"]("+api.get('users/'+r['players'][0]['id'])['weblink']+") got a new WR in ["+c+"]("+api.get('categories/'+r['category'])['weblink']+") with a time of ["+t+"]("+api.get("runs/"+i)['weblink']+")"
-			        elif p ==0:
-			        	m = "["+n+"]("+api.get('users/'+r['players'][0]['id'])['weblink']+") got a new PB in ["+c+"]("+api.get('categories/'+r['category'])['weblink']+") with a time of ["+t+"]("+api.get("runs/"+i)['weblink']+")"
-			        else:
-			        	m = "["+n+"]("+api.get('users/'+r['players'][0]['id'])['weblink']+") got a new PB in ["+c+"]("+api.get('categories/'+r['category'])['weblink']+") with a time of ["+t+"]("+api.get("runs/"+i)['weblink']+") ["+ordinal(p)+"]"
-			        em = discord.Embed(colour=discord.Colour(0xffd700), url="https://discordapp.com", description=m)
-			        await channel.send(embed = em)
+	print("Bot Online!")
+	print("Name: {}".format(client.user.name))
+	print("ID: {}".format(client.user.id))
+	p = check()
+	if p:
+		change()
+		await post()	
+
+multiruns = ['n2y39y1d', 'xk9nemg2', '7kjq8nzd', 'wdml1wek', 'vdo8g8ok']
+HP4 = ['zd3yx82n', '7dgjp4d4']
+HP5 = ['q25o9r8k', 'jdrq8jxk']
+lego = ['wk6jnqd1', 'n2y1oz2o', '7kjr3n23', 'xk9lr4k0', 'ndx807ok', 'w20z5vjd', 'wdmq7le2', 'vdo4n862']
+subcat = ['n2y39y1d', 'xk9nemg2', '7kjq8nzd', 'wdml1wek', 'vdo8g8ok', 'zd3yx82n', '7dgjp4d4', 'q25o9r8k', 'jdrq8jxk', 'wk6jnqd1', 'n2y1oz2o', '7kjr3n23', 'xk9lr4k0', 'ndx807ok', 'w20z5vjd', 'wdmq7le2', 'vdo4n862']
+
+async def post():
+	channel = client.get_channel("******") #active channel
+	channel2 = client.get_channel("******") #test channel
+	t2 = dtime.utcnow()-tdel(minutes = 15)
+	new_runs = []
+	for board in boards:
+		try:
+			cat = api.get("runs?category="+board+"&max=500")
+			for j in cat:
+				if (j['status']['status']=='verified' and j['status']['verify-date'] is not None):
+					vtime = dtime.strptime(j['status']['verify-date'],'%Y-%m-%dT%H:%M:%SZ')
+					if (vtime>=t2):
+						new_runs.append(j['id'])
+		except:
+			await channel2.send("category " + board + "has been deleted")
+	if len(new_runs)>0:
+		for i in new_runs:
+			p = 0
+			r = api.get("runs/"+i)
+			n = api.get('users/'+r['players'][0]['id'])['names']['international']
+			g = api.get('games/'+r['game'])['names']['international']
+			c = g+" "+api.get('categories/'+r['category'])['name']
+			if r['category'] in subcat:
+				try:
+					if r['category'] in multiruns:
+						c = c + " "+api.get("variables/789k439l")['values']['values'][r['values']['789k439l']]['label']
+					if r['category'] in HP4:
+						c = c + " "+api.get("variables/j84k0x2n")['values']['values'][r['values']['j84k0x2n']]['label']
+					if r['category'] in HP5:
+						c = c + " "+api.get("variables/0nw7gk8q")['values']['values'][r['values']['0nw7gk8q']]['label']
+					if r['category'] in lego:
+						c = c + " "+api.get("variables/"+str(list(r['values'].keys())[0]))['values']['values'][r['values'][str(list(r['values'].keys())[0])]]['label']
+				except:
+					await channel2.send("run " + i + " has broken subcategories")
+			t = convert(int(r['times']['primary_t'])).strftime('%H:%M:%S')
+			if r['level'] is None:
+				b = api.get('leaderboards/'+r['game']+'/category/'+r['category'])
+				for j in range(len(b['runs'])):
+					if b['runs'][j]['run']['id']==i:
+						p = b['runs'][j]['place']
+				if p ==1:
+					m = "<:GoldScar:619662499381379072> ["+n+"]("+api.get('users/'+r['players'][0]['id'])['weblink']+") got a new WR in ["+c+"]("+api.get('categories/'+r['category'])['weblink']+") with a time of ["+t+"]("+api.get("runs/"+i)['weblink']+")"
+				elif p ==0:
+					m = "["+n+"]("+api.get('users/'+r['players'][0]['id'])['weblink']+") got a new PB in ["+c+"]("+api.get('categories/'+r['category'])['weblink']+") with a time of ["+t+"]("+api.get("runs/"+i)['weblink']+")"
+				else:
+					m = "["+n+"]("+api.get('users/'+r['players'][0]['id'])['weblink']+") got a new PB in ["+c+"]("+api.get('categories/'+r['category'])['weblink']+") with a time of ["+t+"]("+api.get("runs/"+i)['weblink']+") ["+ordinal(p)+"]"
+				em = discord.Embed(colour=discord.Colour(0xffd700), url="https://discordapp.com", description=m)
+				await channel.send(embed = em)
 	await client.close()
 def sr_handler(event, context):
-	client.run("xxxx")
+	client.run("******") #Bot ID
